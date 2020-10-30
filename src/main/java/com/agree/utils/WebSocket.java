3cd14109ad1340ecf85669aa5305ecb847bf4f1f
wuyongchang <wuyongchang@agree.com.cn>
2020-10-30 17:51:00 +0800

    Remove unused package

--- src/main/java/com/agree/utils/WebSocket.java
+++ src/main/java/com/agree/utils/WebSocket.java
@@ -7,7 +7,6 @@ import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
